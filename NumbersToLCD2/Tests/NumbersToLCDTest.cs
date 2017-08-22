86f2f4c84eac2983edb251eb06c33fc4cd73c8c0
Leah Wynn <leahvwynn@gmail.com>
2017-08-21 21:44:18 -0400

    first commit

---
+++ NumbersToLCD2/Tests/NumbersToLCDTest.cs
@@ -0,0 +1,44 @@
+using System;
+using Xunit;
+using NumbersToLCD2.Production;
+
+namespace NumbersToLCD2
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void NumberOneShouldEqualLCDOne()
+        {
+            NumbersToLCD translateNumber = new NumbersToLCD("1");
+            Assert.Equal("   \n" +
+                         "  |\n" +
+                         "  |", translateNumber.NumberToString());
+
+        }
+
+        [Fact]
+        public void NumberTwoShouldEqualLCDTwo()
+        {
+            NumbersToLCD translateNumber = new NumbersToLCD("2");
+            Assert.Equal(" _ \n" +
+                         " _|\n" +
+                         "|_ ", translateNumber.NumberToString());
+        }
+		[Fact]
+		public void NumberThreeShouldEqualLCDThree()
+		{
+			NumbersToLCD translateNumber = new NumbersToLCD("3");
+			Assert.Equal(" _ \n" +
+						 " _|\n" +
+						 " _|", translateNumber.NumberToString());
+		}
+        [Fact]
+        public void DoubleDigitIntShouldReturnDoubleDigitLCD()
+        {
+            NumbersToLCD translateNumber = new NumbersToLCD("12");
+            Assert.Equal("    _ \n" +
+                         "  | _|\n" +
+                         "  ||_ ", translateNumber.NumberToString());
+        }
+    }
+}
